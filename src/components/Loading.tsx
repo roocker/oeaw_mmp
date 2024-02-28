2d64c915eaf2c383cb9039aca9bc15b374ecc9d6
roocker <flosspam@gmail.com>
2024-02-28 21:00:58 +0100

    new: loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="z-10 fixed w-screen h-screen top-0 left-0 flex justify-center items-center backdrop-blur-sm  text-black">
+      Loading...
+    </div>
+  );
+}
