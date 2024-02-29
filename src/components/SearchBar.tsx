5998239aaaf04ced140318156093abeadfeee815
roocker <flosspam@gmail.com>
2024-02-29 14:02:38 +0100

    edit: Search Bar description as placeholder Text

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -24,15 +24,18 @@ export default function SearchBar({
           <Label className="absolute bg-white top-[-0.75em] left-5 px-2">
             {label}
           </Label>
-          <Input className="bg-cyan w-full h-24 text-2xl text-white p-3 indent-8  border-2 border-black bg-clip-content rounded-[1em] focus:outline-0" />
-          {description && (
+          <Input
+            className="bg-cyan w-full h-24 text-2xl text-white p-3 indent-8  border-2 border-black bg-clip-content rounded-[1em] focus:outline-0 placeholder:text-white placeholder:opacity-35 "
+            placeholder={description ? description : "Suche"}
+          />
+          {/* {description && (
             <Text
               slot="description"
               className="text-xs text-gray-600 relative ml-4 top-2 "
             >
               {description}
             </Text>
-          )}
+          )} */}
         </SearchField>
       </Form>
     </section>
